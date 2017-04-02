a80c334283e5a8e029e438545c4bfe9f50dea27e
rene <renegolden331@gmail.com>
2017-04-02 20:05:12 +0200

    update validate range of quality_score of cleaner

--- app/models/cleaner.rb
+++ app/models/cleaner.rb
@@ -3,6 +3,7 @@ class Cleaner < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates_inclusion_of :quality_score, in: 0.0..5.0, message: 'This site is only for 0.0 ~ 5.0'
 
   def name
     return first_name + " " + last_name
